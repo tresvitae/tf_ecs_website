7a5ee9fc96eb531c728fbac21b24b17ac096814d
Patryk Futa <patryk.futa@gmail.com>
2021-05-06 13:03:39 +0200

    Reorganize project structure

---
+++ modules/ecs/variables.tf
@@ -0,0 +1,17 @@
+/*
+variable "aws_ecs_ami" {
+  description = "The AMI to seed ECS instances with."
+}
+variable "max_size" {
+  type = number
+}
+variable "min_size" {
+  type = number
+}
+variable "desired_capacity" {
+  type = number
+}
+variable "instance_type" {
+  type = string
+}
+*/
