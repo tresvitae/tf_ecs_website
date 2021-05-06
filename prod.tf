784dc35afe0ccdc474e9012dbb49029744c60427
Patryk Futa <patryk.futa@gmail.com>
2021-05-06 09:58:13 +0200

    Set SG with HTTP and HTTPS

--- prod.tf
+++ prod.tf
@@ -1,3 +1,7 @@
+variable "open_ip" {
+  type = list(string)
+}
+
 provider "aws" {
   profile = "default"
   region = "eu-west-1"
@@ -19,3 +23,32 @@ resource "aws_default_subnet" "default_az2" {
   }
 }
 
+resource "aws_security_group" "prod_web" {
+  name            = "prod_web"
+  description     = "Allow standard htttp and https prots inbound and everything outbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.open_ip
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.open_ip
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.open_ip
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
