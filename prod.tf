333ca8d5d36af05c5fffe9fa4b91225050ab0abf
Patryk Futa <patryk.futa@gmail.com>
2021-05-09 19:54:50 +0200

    Correct Task Definition settings

--- prod.tf
+++ prod.tf
@@ -91,7 +91,7 @@ resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
 }
 
 resource "aws_security_group" "ecs_sg" {
-  name              = "${var.project_name}--${var.environment}--alb"
+  name              = "${var.project_name}--${var.environment}--sg_ecs"
   vpc_id            = aws_default_vpc.default.id
 
   ingress {
@@ -119,12 +119,83 @@ resource "aws_security_group" "ecs_sg" {
 }
 
 
+# LOAD BALANCER
+resource "aws_alb" "ecs-load-balancer" {
+    name                = "ecs-alb"
+    security_groups     = [aws_security_group.ecs_sg.id]
+    subnets             = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_alb_target_group" "ecs-target-group" {
+  name                = "ecs-target-group"
+  port                = "80"
+  protocol            = "HTTP"
+  vpc_id              = aws_default_vpc.default.id
+
+  health_check {
+    healthy_threshold   = "5"
+    unhealthy_threshold = "2"
+    interval            = "30"
+    matcher             = "200"
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = "5"
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_alb_listener" "alb-listener" {
+    load_balancer_arn = "${aws_alb.ecs-load-balancer.arn}"
+    port              = "80"
+    protocol          = "HTTP"
+
+    default_action {
+        target_group_arn = "${aws_alb_target_group.ecs-target-group.arn}"
+        type             = "forward"
+    }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name              = "${var.project_name}--${var.environment}--sg_alb"
+  vpc_id            = aws_default_vpc.default.id
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = var.open_ip
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_security_group_rule" "instance_in_alb" {
+  type                     = "ingress"
+  from_port                = 32768
+  to_port                  = 65535
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.ecs_sg.id
+  security_group_id        = aws_security_group.alb_sg.id
+}
+
+
 # SERVICE
 resource "aws_ecs_service" "web_app" {
   name            = "${var.project_name}--${var.environment}--service"
   cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = aws_ecs_task_definition.softserve.arn
   desired_count   = 2
+  # LOAD BALANCER??????? load_balancer {}
 }
 
 /*
